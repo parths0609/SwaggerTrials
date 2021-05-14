56e6f4e5e8b05bfb46dccd3f2f4feca7f39ef9cd
Parth Shekokar <shekokarparth@gmail.com>
2021-05-14 18:42:51 +0530

    Add project files.

---
+++ SwaggerTrials/Controllers/ValuesController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SwaggerTrials.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {  
+        /// <summary>
+        /// Get action method without any argument
+        /// </summary>
+        /// <returns></returns>
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        /// <summary>
+        /// Get action method that accepts an integer as an argument
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        /// <summary>
+        /// Post action method to add data
+        /// </summary>
+        /// <param name="value"></param>
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+        /// <summary>
+        /// Put action method to modify data
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+        /// <summary>
+        /// Delete action method
+        /// </summary>
+        /// <param name="id"></param>
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+      
+    }
+}
